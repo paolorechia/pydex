32905c6952dda40d92bb25bff1813a923d1f2ae1
paolorechia <paolorechia@gmail.com>
2022-09-20 08:26:04 +0200

    Bootstrap registration frontend

---
+++ signup-frontend/handler.py
@@ -0,0 +1,12 @@
+import jinja2
+
+dev_url = "https://dev.codex.api.openimagegenius.com/redirect"
+dev_url = "https://codex.api.openimagegenius.com/redirect"
+
+
+def signup(event, context):
+    pass
+
+
+def google_redirect(event, context):
+    pass
